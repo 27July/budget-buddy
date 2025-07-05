cf29845695dfeb40c4cf350db54f02e415c451b6
Zi Hao Wee <91110126+27July@users.noreply.github.com>
2025-07-05 22:29:20 +0800

    testing electron renderer

--- electron/main.js
+++ electron/main.js
@@ -1,1 +1,14 @@
-console.log('Testing Electron')
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('electron/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
