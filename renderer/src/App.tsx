f55265cc73e6a3a02c489906dab93002dd4e332d
Zi Hao Wee <91110126+27July@users.noreply.github.com>
2025-07-09 19:13:17 +0800

    install shadcn

--- renderer/src/App.tsx
+++ renderer/src/App.tsx
@@ -1,13 +1,14 @@
 //import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
-
+import { Button } from "@/components/ui/button";
 
 // src/App.tsx
 function App() {
   return (
     <div className="h-screen bg-gradient-to-br from-pink-400 to-purple-500 flex items-center justify-center">
       <h1 className="text-white text-4xl font-bold">🎉 Tailwind is working!</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
