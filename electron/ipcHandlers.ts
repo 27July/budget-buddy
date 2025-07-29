417f0773ad2ddd198f361900650295951fc4cc16
Zi Hao Wee <91110126+27July@users.noreply.github.com>
2025-07-30 03:44:28 +0800

    ipc handlers

--- electron/ipcHandlers.ts
+++ electron/ipcHandlers.ts
@@ -1,29 +1,80 @@
-import { getTransactions, getAllCategories } from './queries'
-import { addTransaction, addCategory, addRecurringExpense } from './mutation'
-import { deleteTransaction, deleteCategory } from './deletion';
+import { getTransactions, getAllCategories, getAllRecurringExpenses, getAllBudgets, getCategoriesForBudget, getTransactionById, getBudgetById } from './queries'
+import { addTransaction, addCategory, addRecurringExpense, addBudget, addBudgetCategory, clearBudgetCategory, updateTransaction, updateCategory, updateRecurringExpense, updateBudget } from './mutation'
+import { deleteTransaction, deleteCategory, deleteRecurringExpense, deleteBudget, deleteBudgetCategory } from './deletion';
 import { ipcMain } from 'electron';
 
 
 export function registerIpcHandlers() {
-    ipcMain.handle('get-transactions', async (event, args) => {
-        return getTransactions(args);
-      })
-    ipcMain.handle('add-transaction', async (event, args) => {
-        return addTransaction(args);
+    //Queries
+    ipcMain.handle('get-transactions', async (event, filters) => {
+        return getTransactions(filters);
       })
     ipcMain.handle('get-all-categories', async () => {
-        return getAllCategories();
+    return getAllCategories();
       })
-    ipcMain.handle('add-category', async (event, args) => {
-        return addCategory(args);
+    ipcMain.handle('get-all-recurring-expenses', async () => {
+        return getAllRecurringExpenses();
       })
-    ipcMain.handle('add-recurring-expense', async (event, args) => {
-        return addRecurringExpense(args);
+    ipcMain.handle('get-all-budgets', async () => {
+        return getAllBudgets();
       })
+    ipcMain.handle('get-categories-for-budget', async (event, budgetId) => {
+        return getCategoriesForBudget(budgetId);
+      })
+    ipcMain.handle('get-transaction-by-id', async (event, transactionId) => {
+        return getTransactionById(transactionId);
+      })
+    ipcMain.handle('get-budget-by-id', async (event, budgetId) => {
+        return getBudgetById(budgetId);
+      })
+    
+    //Mutations
+    ipcMain.handle('add-transaction', async (event, transaction) => {
+        return addTransaction(transaction);
+      })
+    
+    ipcMain.handle('add-category', async (event, category) => {
+        return addCategory(category);
+      })
+    ipcMain.handle('add-recurring-expense', async (event, recurringExpense) => {
+        return addRecurringExpense(recurringExpense);
+      })
+    ipcMain.handle('add-budget', async (event, name, amountAllocated) => {
+        return addBudget(name, amountAllocated);
+      })
+    ipcMain.handle('add-budget-category', async (event, budgetId, categoryId) => {
+        return addBudgetCategory(budgetId, categoryId);
+      })
+    ipcMain.handle('clear-budget-category', async (event, budgetId) => {
+        return clearBudgetCategory(budgetId);
+      })
+    ipcMain.handle('update-transaction', async (event, transaction) => {
+        return updateTransaction(transaction);
+      })
+    ipcMain.handle('update-category', async (event, categoryId, name) => {
+        return updateCategory(categoryId, name);
+      })
+    ipcMain.handle('update-recurring-expense', async (event, expense) => {
+        return updateRecurringExpense(expense);
+      })
+    ipcMain.handle('update-budget', async (event, budgetId, name, amountAllocated) => {
+        return updateBudget(budgetId, name, amountAllocated);
+      })
+    
+    //Deletion
     ipcMain.handle('delete-transaction', async (event, transactionId) => {
         return deleteTransaction(transactionId);
       })
     ipcMain.handle('delete-category', async (event, categoryId) => {
         return deleteCategory(categoryId);
+      })
+    ipcMain.handle('delete-recurring-expense', async (event, expenseId) => {
+        return deleteRecurringExpense(expenseId);
+      })
+    ipcMain.handle('delete-budget', async (event, budgetId) => {
+        return deleteBudget(budgetId);
+      })
+    ipcMain.handle('delete-budget-category', async (event, budgetId, categoryId) => {
+        return deleteBudgetCategory(budgetId, categoryId);
       })
 }
